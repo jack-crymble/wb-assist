682c55652c2e74d4b585f0156cfc9ce18e86bc59
Jack Crymble <jack.crymble@gmail.com>
2024-09-25 20:58:41 +0100

    feat: basic page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+export interface Part {
+  value: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'wb-assist';
+  parts: Part[] = [
+    { value: 'flight controller', label: 'Flight controller' },
+    { value: 'esc', label: 'ESC' },
+    { value: 'motor', label: 'Motor' },
+    { value: 'vtx', label: 'VTX' },
+    { value: 'rx', label: 'Rx' },
+    { value: 'propeller', label: 'Propeller' },
+    { value: 'camera', label: 'Camera' },
+    { value: 'gps', label: 'GPS' },
+    { value: 'battery', label: 'Battery' },
+    { value: 'video antenna', label: 'Video Antenna' },
+  ];
 }
